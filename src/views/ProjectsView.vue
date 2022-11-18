0dea9edf658915583f22bc19eda15cc9c71aa9ff
Jonathan <beaubj@uw.edu>
2022-11-18 15:19:35 -0800

    Updated video

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -5,7 +5,7 @@
   <div class="container mx-auto font-mono text-center" ref="container">
     This page is a work in progress. Please enjoy a video demo of DJ Democracy.
     <video class="mx-auto my-4" width="1280" height="960" controls muted>
-      <source src="../assets/DJD.mp4" type="video/mp4" />
+      <source src="../assets/DJD2.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
   </div>
